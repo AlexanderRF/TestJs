fc6157ab733c5722f17cc5e0e16331b1179a6692
Alexander <worldintheworld@mail.ru>
2019-07-15 13:42:50 +0300

    Second commit

--- js.js
+++ js.js
@@ -17,10 +17,10 @@ for (let i = 0; i < 2; i++){
 
     if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
      && a != '' && b != '' && a.length < 50){
-         console.log("done");
+        console.log("done");
         appData.expenses[a] = b;
     } else {
-
+        --i;
     }
 
 };
